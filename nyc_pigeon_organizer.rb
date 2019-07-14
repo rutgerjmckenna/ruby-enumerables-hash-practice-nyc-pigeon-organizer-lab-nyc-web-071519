754c043ced71a7f14b955681aa1eab339e3dd655
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-14 21:50:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons_by_name []
+  new_hash {}
+  
+  data.each do |quality, qual_type|
+    qual_type.each do |qual_type, array|
+      array.each do |name|
+        pigeons_by_name << name
+    end
+  end
 end
+pidgeons_by_name
+end
+
+nyc_pigeon_organizer(pigeon_data)
